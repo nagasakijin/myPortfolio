90c67451832eef6376c730212888139bb371d7e3
nagasakijin <sekaihaitumotekitouni@gmail.com>
2023-07-25 10:45:11 +0900

    add:画像挿入

---
+++ js/portfolio.js
@@ -0,0 +1,10 @@
+//sp用ハンバーガーナビゲーションの実装
+$(".open_btn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".open_btn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
