36a917d80f6e22c45e3934f38b437aece7ed1216
nagasakijin <sekaihaitumotekitouni@gmail.com>
2023-08-17 11:08:17 +0900

    JSトップへ戻る設置（アニメーションへの変更予定）

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,18 @@
 //sp用ハンバーガーナビゲーションの実装
-$(".open_btn").click(function () {//ボタンがクリックされたら
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-});
-
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".open_btn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+$(function(){
+    var pagetop = $('#page_top');
+    // ボタン非表示
+    pagetop.hide();
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+       }
+    });
+    pagetop.click(function () {
+       $('body, html').animate({ scrollTop: 0 }, 500);
+       return false;
+    });
 });
