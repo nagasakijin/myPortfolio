3d825a7e144a4c5a5b6f55fc3a10b822b73a7c13
nagasakijin <sekaihaitumotekitouni@gmail.com>
2023-09-19 07:08:22 +0900

    ハンバーガー実装

--- js/portfolio.js
+++ js/portfolio.js
@@ -16,3 +16,12 @@ $(function(){
        return false;
     });
 });
+$(".open_btn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g_nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g_nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".open_btn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g_nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
